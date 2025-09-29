93ee236f6ebba70158819461933902002dd80d58
lokeshpatil84 <lokeshpatil8484@gmail.com>
2025-09-29 21:27:37 +0530

    update file code

--- slices/slices.go
+++ slices/slices.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	cart := []string{"apple", "bannana", "grape"}
@@ -30,5 +33,80 @@ func main() {
 	fmt.Println(fruit1)
 
 	//---------------------------------
+	var s []int
+	for i := range 10000 {
+		s = appendInt(s, i)
+	}
+	fmt.Println(s[:10])
+	//---------------------------------
+	// Ecercise: cncat ,without using a "for" loop
+	out := concat([]string{"a", "b"}, []string{"c"})
+	fmt.Println(out)
 
+	//---------------------------------
+	// Ecercise: Medain
+	values:= []float64{1,6,3,4,5}
+    value1:= []float64{1,2,3,5}
+	fmt.Println(median(values))
+	fmt.Println(median(value1))
+	fmt.Println(values)
+
+	//---------------------------------
+
+	players:= []player{
+		{"lokesh",10000000},
+		{"dhoni",40},
+	}
+
+
+	
+	// add bonus
+	//pointer semantics "or" loop
+	for i:= range players{
+		players[i].Score+=100
+	}
+	fmt.Println(players)
+}
+
+type player struct{
+	Name string
+	Score int
+}
+
+func median(values []float64) float64 {
+	  
+    vals := make([]float64, len(values))
+	copy(vals, values)
+	   sort.Float64s(vals)
+	    length:= len(vals)/2
+	   if len(vals)%2==1{
+		   return vals[length]
+	   }
+
+	  mid := (vals[length-1] + vals[length])/2
+	  return mid
+
+	}
+
+
+func concat(a, b []string) []string {
+	cont := make([]string, len(a)+len(b))
+	copy(cont, a)
+	copy(cont[len(a):], b)
+
+	return cont
+}
+
+func appendInt(s []int, v int) []int {
+	i := len(s)
+	if len(s) == cap(s) {
+		size := 2 * (len(s) + 1)
+		fmt.Println(cap(s), "->", size)
+		new := make([]int, size)
+		copy(new, s)
+		s = new[:len(s)]
+	}
+	s = s[:len(s)+1]
+	s[i] = v
+	return s
 }
